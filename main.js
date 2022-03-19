8bc92dffc314fe3a471c798e41e1901131f425df
AngelicaCorrales <angelicacorrales2002@gmail.com>
2022-03-19 11:57:01 -0500

    IT11-12 Implementar la funcionalidad de cerrar issue

--- main.js
+++ main.js
@@ -58,4 +58,18 @@ function fetchIssues () {
         }
     }
 }
+
+function setStatusClosed (id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  
+  for(var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
+    }
+  }
+    
+  localStorage.setItem('issues', JSON.stringify(issues));
+  
+  fetchIssues();
+}
   
